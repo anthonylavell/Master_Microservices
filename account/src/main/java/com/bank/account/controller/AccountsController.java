9c1233b937e778058f2219db0dd06c9f183b33d5
anthonylavell <anthonylavel@gmail.com>
2022-01-04 22:41:54 -0600

    Add code client depends to account

--- account/src/main/java/com/bank/account/controller/AccountsController.java
+++ account/src/main/java/com/bank/account/controller/AccountsController.java
@@ -1,23 +1,40 @@
 package com.bank.account.controller;
 
+import com.bank.account.config.AccountServiceConfig;
 import com.bank.account.entities.Accounts;
 import com.bank.account.entities.Customer;
+import com.bank.account.entities.Properties;
 import com.bank.account.repository.AccountsRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 public class AccountsController {
     @Autowired
     private AccountsRepository accountsRepository;
 
+    @Autowired
+    AccountServiceConfig accountsConfig;
+
     @PostMapping("/myAccount")
     public Accounts getCardDetails(@RequestBody Customer customer){
         Accounts account = accountsRepository.findByCustomerId(customer.getCustomerId());
         return account;
+    }
+
+    @GetMapping("account/properties")
+    public String getPropertyDetails() throws JsonProcessingException {
+        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+
+        Properties properties = new Properties(accountsConfig.getMsg(),accountsConfig.getBuildVersion(),
+                accountsConfig.getMailDetails(), accountsConfig.getActiveBranches());
+        String jsonStr = ow.writeValueAsString(properties);
+        return jsonStr;
     }
 }
